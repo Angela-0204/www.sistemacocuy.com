8a6e37efe74757bba342ae321b6634a6e9591646
daniel05lucena <daniellucenadc@gmail.com>
2024-07-14 11:06:59 -0400

    cambio_agendar_pedido

--- views/catalogo.php
+++ views/catalogo.php
@@ -89,7 +89,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-        <button type="button" class="btn btn-primary" id="addProductButton">Agregar</button>
+        <button type="button" class="btn btn-primary" id="addProductButton">Agendar Pedido</button>
       </div>
     </div>
   </div>
