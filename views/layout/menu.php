8402a952db41afe976992cf727c7233be2c999ef
Angela-0204 <angelarojas0204@gmail.com>
2024-11-14 16:55:42 -0400

    manual de usuario

--- views/layout/menu.php
+++ views/layout/menu.php
@@ -335,6 +335,27 @@
     </li>
     <?php } ?>
 
+    <li class="nav-item">
+    <a href="#" class="nav-link active">
+        <i class="nav-icon fas fa-notes-medical"></i>
+        <p>
+            Ayuda
+            <i class="right fas fa-angle-left"></i>
+        </p>
+    </a>
+    <ul class="nav nav-treeview">
+        <li class="nav-item">
+            <a href="manual.pdf" class="nav-link active" target="_blank">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Manual de usuario</p>
+            </a>
+        </li>
+    </ul>
+</li>
+
+
+    
+
     <!-- Cerrar Sesión -->
     <li class="nav-item">
         <a href="?pagina=logout" class="nav-link" style="background-color: #C82333">
